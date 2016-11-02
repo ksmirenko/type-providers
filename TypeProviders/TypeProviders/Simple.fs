4fe2387385fa5c766cf2de157c56c2e41eab6cae
Kirill Smirenko <k.smirenko@gmail.com>
2016-11-02 19:44:52 +0300

    Initial commit, resolve dependencies for empty project

---
+++ TypeProviders/TypeProviders/Simple.fs
@@ -0,0 +1,11 @@
+﻿module TypeProviders.Simple
+
+open ProviderImplementation.ProvidedTypes
+open Microsoft.FSharp.Core.CompilerServices
+
+[<TypeProvider>]
+type SimpleProvider (config : TypeProviderConfig) as this =
+    inherit TypeProviderForNamespaces ()
+
+[<assembly:TypeProviderAssembly>]
+do ()
