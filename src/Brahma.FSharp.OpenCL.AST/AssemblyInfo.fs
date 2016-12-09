39fd328db8c47525c47f7c0392fbc8f0bcd7a1c4
Kirill Smirenko <k.smirenko@gmail.com>
2016-12-09 20:20:42 +0300

    Add AST, add paket, create a project for OpenCL provider

---
+++ src/Brahma.FSharp.OpenCL.AST/AssemblyInfo.fs
@@ -0,0 +1,13 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("Brahma.FSharp.OpenCL.AST")>]
+[<assembly: AssemblyProductAttribute("Brahma.FSharp")>]
+[<assembly: AssemblyDescriptionAttribute("F# quotation to OpenCL translator.")>]
+[<assembly: AssemblyVersionAttribute("1.0.1")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.1")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0.1"
+    let [<Literal>] InformationalVersion = "1.0.1"
---
+++ src/Brahma.FSharp.OpenCL.AST/README.md
@@ -0,0 +1,3 @@
+﻿This project was copied from [Brahma.FSharp](https://github.com/YaccConstructor/Brahma.FSharp).
+
+See [LICENSE.txt](LICENSE.txt) for license information.
