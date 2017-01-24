848356eea7e1e507b294671f7aeff0fd3cbd8ca5
Kirill Smirenko <k.smirenko@gmail.com>
2017-01-24 20:44:30 +0300

    Add some real world tests

---
+++ tests/OpenCLTranslator.Test/RealWorldTests.fs
@@ -0,0 +1,121 @@
+﻿namespace OpenCLTranslator.Test
+
+open NUnit.Framework
+open OpenCLTranslator.Main
+open Brahma.FSharp.OpenCL.AST
+
+[<TestFixture>]
+type RealWorldTests() =
+    let emptyBody = new StatementBlock<_>(new ResizeArray<_>())
+
+    let testSuccess code res =
+        let actualRes = parseCLCode code
+        Assert.AreEqual(res, actualRes)
+
+    [<Test>]
+    member this.``Matrix * matrix``() =
+        let code = "__kernel void myGEMM1(const int M, const int N, const int K,\n\
+                                          const __global float* A,\n\
+                                          const __global float* B,\n\
+                                          __global float* C) {\n\
+                        // Thread identifiers\n\
+                        const int globalRow = get_global_id(0); // Row ID of C (0..M)\n\
+                        const int globalCol = get_global_id(1); // Col ID of C (0..N)\n\
+                        // Compute a single element (loop over K)\n\\n\
+                        float acc = 0.0f;\n\
+                        for (int k=0; k<K; k++) {\n\
+                            acc += A[k*M + globalRow] * B[globalCol*K + k];\n\
+                        }\n\\n\
+                        // Store the result\n\
+                        C[globalCol*M + globalRow] = acc;\n\
+                    }"
+        let f = FunDecl<Lang>(
+                    DeclSpecifierPack<Lang>(
+                        funQual=Kernel,
+                        typeSpec=PrimitiveType<Lang>(Void)
+                    ),
+                    "myGEMM1",
+                    [
+                        FunFormalArg<Lang>(
+                            DeclSpecifierPack<Lang>(typeSpec=PrimitiveType<Lang>(Int), typeQuals=[TypeQualifier.Const]),
+                            "M");
+                        FunFormalArg<Lang>(
+                            DeclSpecifierPack<Lang>(typeSpec=PrimitiveType<Lang>(Int), typeQuals=[TypeQualifier.Const]),
+                            "N");
+                        FunFormalArg<Lang>(
+                            DeclSpecifierPack<Lang>(typeSpec=PrimitiveType<Lang>(Int), typeQuals=[TypeQualifier.Const]),
+                            "K");
+                        FunFormalArg<Lang>(
+                            DeclSpecifierPack<Lang>(
+                                addrSpaceQual=Global,
+                                typeSpec=RefType<Lang>(PrimitiveType<Lang>(Float), []),
+                                typeQuals=[TypeQualifier.Const]
+                            ),
+                            "A");
+                        FunFormalArg<Lang>(
+                            DeclSpecifierPack<Lang>(
+                                addrSpaceQual=Global,
+                                typeSpec=RefType<Lang>(PrimitiveType<Lang>(Float), []),
+                                typeQuals=[TypeQualifier.Const]
+                            ),
+                            "B");
+                        FunFormalArg<Lang>(
+                            DeclSpecifierPack<Lang>(
+                                addrSpaceQual=Global,
+                                typeSpec=RefType<Lang>(PrimitiveType<Lang>(Float), [])
+                            ),
+                            "C")
+                    ],
+                    emptyBody
+                )
+        testSuccess code [f]
+
+    [<Test>]
+    member this.``Matrix * vector``() =
+        let code = "__kernel void matvec(__global const float *A,\n\
+                                         __global const float *x,\n\
+                                         uint ncols,\n\
+                                         __global float *y)\n\
+                   {\n\
+                       size_t i = get_global_id(0);\n\
+                       __global float const *a = &A[i*ncols];\n\
+                       float sum = 0.f;\n\
+                       for (size_t j = 0; j < ncols; j++) {\n\
+                           sum += a[j] * x[j];\n\
+                       }\n\
+                       y[i] = sum;\n\
+                   }"
+        let f = FunDecl<Lang>(
+                    DeclSpecifierPack<Lang>(
+                        funQual=Kernel,
+                        typeSpec=PrimitiveType<Lang>(Void)
+                    ),
+                    "matvec",
+                    [
+                        FunFormalArg<Lang>(
+                            DeclSpecifierPack<Lang>(
+                                addrSpaceQual=Global,
+                                typeSpec=RefType<Lang>(PrimitiveType<Lang>(Float), []),
+                                typeQuals=[TypeQualifier.Const]
+                            ),
+                            "A");
+                        FunFormalArg<Lang>(
+                            DeclSpecifierPack<Lang>(
+                                addrSpaceQual=Global,
+                                typeSpec=RefType<Lang>(PrimitiveType<Lang>(Float), []),
+                                typeQuals=[TypeQualifier.Const]
+                            ),
+                            "x");
+                        FunFormalArg<Lang>(
+                            DeclSpecifierPack<Lang>(typeSpec=PrimitiveType<Lang>(UInt)),
+                            "ncols");
+                        FunFormalArg<Lang>(
+                            DeclSpecifierPack<Lang>(
+                                addrSpaceQual=Global,
+                                typeSpec=RefType<Lang>(PrimitiveType<Lang>(Float), [])
+                            ),
+                            "y")
+                    ],
+                    emptyBody
+                )
+        testSuccess code [f]
