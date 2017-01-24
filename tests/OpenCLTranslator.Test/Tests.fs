55232793713c90ebe9e5d9bc10bc4e216c885608
Kirill Smirenko <k.smirenko@gmail.com>
2017-01-24 17:51:12 +0300

    Add more tests, fix parsing C identifiers in lexer, sort type qualifiers in parser

--- tests/OpenCLTranslator.Test/Tests.fs
+++ tests/OpenCLTranslator.Test/Tests.fs
@@ -10,17 +10,32 @@ type TestClass() =
 
     let testSuccess code res =
         let actualRes = parseCLCode code
-        Assert.True(res.Equals(actualRes))
+        Assert.AreEqual(res, actualRes)
 
     [<Test>]
     member this.``Single parameterless kernel``() =
-        let f1 = FunDecl<Lang>(
+        let code = "__kernel void epicKernel() { return; }"
+        let f = FunDecl<Lang>(
                     DeclSpecifierPack<Lang>(funQual=Kernel, typeSpec=PrimitiveType<Lang>(Void)),
                     "epicKernel",
                     [],
-                    emptyBody 
-                  )
-        testSuccess "__kernel void epicKernel() { return; }" [f1]
+                    emptyBody
+                 )
+        testSuccess code [f]
+
+    [<Test>]
+    member this.``Function body with inner braces``() =
+        let code = "__kernel void epicKernel() {\n\
+                    int ncols = 6, sum = 0;\n\
+                    for (size_t j = 0; j < ncols; j++) { sum += 1; }\n\
+                    return; }"
+        let f = FunDecl<Lang>(
+                    DeclSpecifierPack<Lang>(funQual=Kernel, typeSpec=PrimitiveType<Lang>(Void)),
+                    "epicKernel",
+                    [],
+                    emptyBody
+                 )
+        testSuccess code [f]
 
     [<Test>]
     member this.``Three functions in one file``() =
@@ -31,18 +46,103 @@ type TestClass() =
                     DeclSpecifierPack<Lang>(funQual=Kernel, typeSpec=PrimitiveType<Lang>(Void)),
                     "epicKernel1",
                     [],
-                    emptyBody 
-                  )
+                    emptyBody
+                 )
         let f2 = FunDecl<Lang>(
                     DeclSpecifierPack<Lang>(funQual=Kernel, typeSpec=PrimitiveType<Lang>(Void)),
                     "epicKernel2",
                     [FunFormalArg<Lang>(DeclSpecifierPack<Lang>(typeSpec=PrimitiveType<Lang>(Int)), "bar")],
-                    emptyBody 
-                  )
+                    emptyBody
+                 )
         let f3 = FunDecl<Lang>(
                     DeclSpecifierPack<Lang>(typeSpec=PrimitiveType<Lang>(Float)),
                     "epicFun",
                     [FunFormalArg<Lang>(DeclSpecifierPack<Lang>(typeSpec=PrimitiveType<Lang>(Double)), "baz")],
-                    emptyBody 
-                  )
+                    emptyBody
+                 )
         testSuccess code [f1; f2; f3]
+
+    [<Test>]
+    member this.``Extern kernel with pointer parameters``() =
+        let code = "extern kernel void foo(int *a, char* *b);"
+        let f = FunDecl<Lang>(
+                    DeclSpecifierPack<Lang>(
+                        funQual=Kernel,
+                        storClassSpec=Extern,
+                        typeSpec=PrimitiveType<Lang>(Void)
+                    ),
+                    "foo",
+                    [
+                        FunFormalArg<Lang>(
+                            DeclSpecifierPack<Lang>(typeSpec=RefType<Lang>(PrimitiveType<Lang>(Int), [])),
+                            "a");
+                        FunFormalArg<Lang>(
+                            DeclSpecifierPack<Lang>(
+                                typeSpec=RefType<Lang>(RefType<Lang>(PrimitiveType<Lang>(Char), []), [])),
+                            "b")
+                    ],
+                    emptyBody
+                )
+        testSuccess code [f]
+
+    [<Test>]
+    member this.``Distinct modifiers, signed/unsigned parameters``() =
+        let code = "extern kernel void foo(global volatile restrict signed int *a,\
+                    __constant volatile unsigned long *b);"
+        let f = FunDecl<Lang>(
+                    DeclSpecifierPack<Lang>(
+                        funQual=Kernel,
+                        storClassSpec=Extern,
+                        typeSpec=PrimitiveType<Lang>(Void)
+                    ),
+                    "foo",
+                    [
+                        FunFormalArg<Lang>(
+                            DeclSpecifierPack<Lang>(
+                                addrSpaceQual=Global,
+                                typeSpec=RefType<Lang>(PrimitiveType<Lang>(Int), []),
+                                typeQuals=[ TypeQualifier.Restrict; TypeQualifier.Volatile ]
+                            ),
+                            "a"
+                        );
+                        FunFormalArg<Lang>(
+                            DeclSpecifierPack<Lang>(
+                                addrSpaceQual=Constant,
+                                typeSpec=RefType<Lang>(PrimitiveType<Lang>(ULong), []),
+                                typeQuals=[ TypeQualifier.Volatile ]
+                            ),
+                            "b"
+                        )
+                    ],
+                    emptyBody
+                )
+        testSuccess code [f]
+
+    [<Test>]
+    member this.``Sneaky pointers, unsigned``() =
+        let code = "unsigned int foo(const int *ptr_to_constant, unsigned int *const constant_ptr)\n\
+                    { return *ptr_to_constant; }"
+        let f = FunDecl<Lang>(
+                    DeclSpecifierPack<Lang>(
+                        typeSpec=PrimitiveType<Lang>(UInt)
+                    ),
+                    "foo",
+                    [
+                        FunFormalArg<Lang>(
+                            DeclSpecifierPack<Lang>(
+                                typeSpec=RefType<Lang>(PrimitiveType<Lang>(Int), []),
+                                typeQuals=[ TypeQualifier.Const ]
+                            ),
+                            "ptr_to_constant"
+                        );
+                        FunFormalArg<Lang>(
+                            DeclSpecifierPack<Lang>(
+                                typeSpec=RefType<Lang>(PrimitiveType<Lang>(UInt), [ TypeQualifier.Const ]),
+                                typeQuals=[]
+                            ),
+                            "constant_ptr"
+                        )
+                    ],
+                    emptyBody
+                )
+        testSuccess code [f]
